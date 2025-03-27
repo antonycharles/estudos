9f936654b3ecf62b64a9f6265d2856bc05bfd76a
Antony Reis <antony.reis.dev@gmail.com>
2025-03-26 22:08:13 -0300

    alteração vetor

--- estrutura-de-dados/Vetores/Program.cs
+++ estrutura-de-dados/Vetores/Program.cs
@@ -1,8 +1,19 @@
 ﻿using Vetores;
 
 var vetor = new Vetor(5);
-vetor.Adiciona("elemento 1");
 vetor.Adiciona("elemento 2");
-vetor.Adiciona("elemento 3");
+vetor.Adiciona("elemento 4");
+vetor.Adiciona("elemento 5");
+vetor.ListaElementos();
+Console.WriteLine("Adicona posição:");
+vetor.Adiciona(0,"elemento 1");
+vetor.ListaElementos();
+Console.WriteLine("Adicona posição:");
+vetor.Adiciona(2,"elemento 3");
+vetor.ListaElementos();
+Console.WriteLine("aumenta capacidade:");
+vetor.Adiciona("elemento 6");
+vetor.Adiciona("elemento 7");
+vetor.ListaElementos();
 
-Console.WriteLine(vetor.Busca(5));
+Console.WriteLine(vetor.GetTamanho());
--- estrutura-de-dados/Vetores/Vetor.cs
+++ estrutura-de-dados/Vetores/Vetor.cs
@@ -17,12 +17,38 @@ namespace Vetores
         }
 
         public void Adiciona(string elemento){
+            aumentaCapacidade();
             if(this.tamanho < this.elementos.Length){
                 this.elementos[this.tamanho] = elemento;
                 this.tamanho++;
             } else {
                 throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos.");
             }
+        }
+
+        public void Adiciona(int posicao, string elemento){
+            aumentaCapacidade();
+            if(posicao > this.tamanho || posicao < 0)
+                throw new Exception("Posíção inválida");
+
+            for(int i = this.tamanho; i > posicao ;i--){
+                this.elementos[i] = this.elementos[i-1];
+            }
+
+            this.elementos[posicao] = elemento;
+            this.tamanho++;
+        }
+
+        private void aumentaCapacidade(){
+            if(this.tamanho+1 <= this.elementos.Length)
+                return;
+
+            var novoElemento = new string[this.elementos.Length + 1];
+
+            for(var i = 0; i < this.elementos.Length;i++)
+                novoElemento[i] = this.elementos[i];
+
+            this.elementos = novoElemento;
         }
 
         public int BuscaPosicao(string elemento){
@@ -41,6 +67,10 @@ namespace Vetores
             }
 
             return this.elementos[posicao];
+        }
+
+        public int GetTamanho(){
+            return this.tamanho;
         }
 
         public void ListaElementos(){
